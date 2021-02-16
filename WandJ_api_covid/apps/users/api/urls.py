ba183f873f4abb43663a3f26baa2fdc0311f41c8
Nicolás <euclidesnoeuclides@gmail.com>
2021-02-15 22:08:04 -0500

    modifying views, serializers and endpoint to handle login and register

--- WandJ_api_covid/apps/users/api/urls.py
+++ WandJ_api_covid/apps/users/api/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
-from apps.users.api.api import UserAPIView, UserDetailAPIView
+from django.urls import path, include
+from apps.users.api.api import (
+    UserAPIView, UserDetailAPIView, LoginAPIView, UserRetrieveView
+)
+from knox.views import LogoutView
+
 
 urlpatterns = [
     path('user/', UserAPIView.as_view()),
     path('user/<int:pk>/', UserDetailAPIView.as_view()),
+    path('', include('knox.urls')),
+    path('user', UserAPIView.as_view()),
+    path('auth/user', UserRetrieveView.as_view()),
+    path('register', UserAPIView.as_view()),
+    path('login', LoginAPIView.as_view()),
+    path('logout', LogoutView.as_view(), name='knox_logout')
 ]
